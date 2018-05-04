d9d5a132ce26f04522b3036632c555dc928b3c39
chamc1984 <chamc1984@gmail.com>
2018-05-04 13:35:59 +0900

    test commit

--- src/main/java/sample/maven_sample/logic/FirstLogic.java
+++ src/main/java/sample/maven_sample/logic/FirstLogic.java
@@ -7,6 +7,7 @@ public class FirstLogic {
 	public static void main(String[] args) {
 		FirstDto dto = new FirstDto();
 		
+		// test Lombok
 		dto.setId(12L);
 		
 		System.out.println(dto.getId());
